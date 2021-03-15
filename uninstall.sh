32a8cf46268e24f1d3897e36f862330844d5d3db
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2021-03-15 16:34:07 -0400

    minor edits

--- uninstall.sh
+++ uninstall.sh
@@ -12,7 +12,8 @@ sudo systemctl stop asus_l410m_wmi_keys
 sudo systemctl disable asus_l410m_wmi_keys
 
 # delete files from location
-sudo rm /usr/bin/asus_l410m_wmi_keys.py
-sudo rm /lib/systemd/system/asus_l410m_wmi_keys.service
+sudo rm -rf /usr/bin/asus_l410m_wmi_keys.py
+sudo rm -rf /lib/systemd/system/asus_l410m_wmi_keys.service
+sudo rm -rf /var/log//asus_l410m_wmi_keys.log
 
 # -)
