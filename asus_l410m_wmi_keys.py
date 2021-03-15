4b0a76590a0807df9ccd0b4940c5b292176625c7
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2021-03-15 16:35:35 -0400

    remove camera code (for now)

--- asus_l410m_wmi_keys.py
+++ asus_l410m_wmi_keys.py
@@ -10,8 +10,8 @@
 #imports
 import fcntl
 import libevdev
+import logging
 import os
-import sys
 import time
 
 # constants
@@ -19,100 +19,63 @@ import time
 KEY_WMI_CAMERA = 133
 KEY_WMI_MYASUS = 134
 
-# get cam vendor/product id
-webcam_found = False
-if os.path.exists('/proc/bus/input/devices'):
-    with open('/proc/bus/input/devices', 'r') as f:
-        lines = f.readlines()
+# set up logging
+logging.basicConfig(filename = '/var/log/asus_l410m_wmi_keys.log',
+    level = logging.DEBUG, format = '%(asctime)s - %(message)s')
 
-        # walk through the file backward (vendor and product come before the name)
-        for line in reversed(lines):
-            if 'WEBCAM' in line.upper():
-                webcam_found = True
-                continue
-            if webcam_found:
-                parts = line.split(' ')
-                vendor = parts[2]
-                vendor = vendor.split('=')
-                vendor = vendor[1]
-                product = parts[3]
-                product = product.split('=')
-                product = product[1]
-                break
-
-# get cam bus/device
-if webcam_found:
-
-    # find all matching vendors
-    vendor_list = []
-    if os.path.exists('/sys/bus/usb/devices/'):
-        possible_ids = os.listdir('/sys/bus/usb/devices/')
-        for id in possible_ids:
-            newfile = '/sys/bus/usb/devices/' + id + '/idVendor'
-            if os.path.exists(newfile):
-                with open(newfile, 'r') as f:
-                    lines = f.readlines()
-                    for line in lines:
-                        if vendor in line:
-                            vendor_list.append(id)
-                            break
-
-    # find all matching products in all matching vendors
-    product_list = []
-    for id in vendor_list:
-        newfile = '/sys/bus/usb/devices/' + id + '/idProduct'
-        if os.path.exists(newfile):
-            with open(newfile, 'r') as f:
-                lines = f.readlines()
-                for line in lines:
-                    if product in line:
-                        product_list.append(id)
-                        break
-
-# make sure there is only one camera entry
-webcam_id = -1
-if len(product_list) > 1:
-    print('More than one camera found, disabling camera key')
-elif len(product_list) == 0:
-    print('No camera found, disabling camera key')
-else:
-
-    # grab the only matchng vendor/product
-    webcam_id = product_list[0]
-
-# get current cam state
-webcam_state = False
-if webcam_id != -1:
-    cam_file = '/sys/bus/usb/devices/' + webcam_id + '/bConfigurationValue'
-    if os.path.exists(cam_file):
-        with open(cam_file, 'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                if '1' in line:
-                    webcam_state = True
-                    break
-    else:
-        print('Could not read cam state, disabling camera key')
-        webcam_id = -1
+# just let users know its starting
+logging.debug('---------------------------------------------------------------')
+logging.debug('Starting script')
 
 # find WMI keyboard
+wmi_kbd_found = False
 wmi_kbd_id = -1
+
+# check if file exists
 if os.path.exists('/proc/bus/input/devices'):
+
+    # read file
     with open('/proc/bus/input/devices', 'r') as f:
         lines = f.readlines()
+
+        # walk through the file to find hotkeys device
         for line in lines:
-            if 'Sysfs=/devices/platform/asus-nb-wmi/input/' in line:
-                parts = line.split('/')
-                part = parts[-1]
-                parts = part.split('input')
-                wmi_kbd_id = parts[1].rstrip()
-                break
+            if 'NAME="ASUS WMI HOTKEYS"' in line.upper():
+
+                # keep walking
+                wmi_kbd_found = True
+                continue
+
+            # found WMI device
+            if wmi_kbd_found:
+
+                # keep walking to find handlers line
+                if 'HANDLERS' in line.upper():
+
+                    # save everything after equals sign
+                    handlers = line.split('=')[-1]
+
+                    # get array of handlers
+                    handler_array = handlers.split(' ')
+
+                    # find event
+                    for handler in handler_array:
+                        if 'EVENT' in handler.upper():
+                            event = handler.upper().split('EVENT')
+
+                            # save it
+                            wmi_kbd_id = event[-1]
+                            break
+
+                    # no more to do here
+                    break
 
 # no keyboard, no laundry
-if wmi_kbd_id == -1:
-    print('WMI keyboard not found, freaking out...')
+if not wmi_kbd_found or wmi_kbd_id == -1:
+    logging.debug('WMI keyboard not found, freaking out...')
     sys.exit(1)
 
+# grad the keyboard
 if os.path.exists('/dev/input/event' + str(wmi_kbd_id)):
 
     # create a file descriptor (pipe) for the WMI keyboard
@@ -132,10 +95,10 @@ if os.path.exists('/dev/input/event' + str(wmi_kbd_id)):
 
 # no keyboard, no laundry
 else:
-    print('Could not open connection to keyboard, freaking out...')
+    logging.debug('Could not open connection to keyboard, freaking out...')
     sys.exit(1)
 
-# create a fake keyboard for the "MyAsus" key
+# create a fake keyboard for the keys
 dev_fake_kbd = libevdev.Device()
 dev_fake_kbd.name = 'Asus_L410M_WMI_Keys'
 dev_fake_kbd.enable(libevdev.EV_KEY.KEY_LEFTSHIFT)
@@ -148,86 +111,60 @@ fake_kbd = dev_fake_kbd.create_uinput_device()
 while True:
 
     # look at each event in the wmi keyboard pipe
-    for e in wmi_kbd.events():
+    for event in wmi_kbd.events():
 
         # if it's the camera key
-        if e.value == KEY_WMI_CAMERA:
+        if event.value == KEY_WMI_CAMERA:
+            logging.debug('Camera key pressed')
+            try:
 
-            # if we found the camera
-            if webcam_id != -1:
+                # press Shift
+                events = [
+                    libevdev.InputEvent(libevdev.EV_KEY.KEY_LEFTSHIFT, 1),
+                    libevdev.InputEvent(libevdev.EV_SYN.SYN_REPORT, 0)
+                ]
+                fake_kbd.send_events(events)
 
-                # toggle the camera state
-                webcam_state = not webcam_state
+                # press Meta
+                events = [
+                    libevdev.InputEvent(libevdev.EV_KEY.KEY_LEFTMETA, 1),
+                    libevdev.InputEvent(libevdev.EV_SYN.SYN_REPORT, 0)
+                ]
+                fake_kbd.send_events(events)
 
-                if os.path.exists('/sys/bus/usb/devices/' + webcam_id +
-                    '/bConfigurationValue'):
+                # press R
+                events = [
+                    libevdev.InputEvent(libevdev.EV_KEY.KEY_R, 1),
+                    libevdev.InputEvent(libevdev.EV_SYN.SYN_REPORT, 0)
+                ]
+                fake_kbd.send_events(events)
 
-                    # get the proper file for the cam
-                    cam_file = open('/sys/bus/usb/devices/' + webcam_id +
-                        '/bConfigurationValue', 'w+')
+                # release R
+                events = [
+                    libevdev.InputEvent(libevdev.EV_KEY.KEY_R, 0),
+                    libevdev.InputEvent(libevdev.EV_SYN.SYN_REPORT, 0)
+                ]
+                fake_kbd.send_events(events)
 
-                    # turn cam on
-                    if webcam_state:
-                        cam_file.write('1')
+                # release Meta
+                events = [
+                    libevdev.InputEvent(libevdev.EV_KEY.KEY_LEFTMETA, 0),
+                    libevdev.InputEvent(libevdev.EV_SYN.SYN_REPORT, 0)
+                ]
+                fake_kbd.send_events(events)
 
-                    # turn cam off
-                    else:
-                        cam_file.write('0')
-
-                    # done with cam file
-                    cam_file.flush()
-                    cam_file.close()
-
-            # no camera, map to Shift-Meta-R
-            else:
-                try:
-
-                    # press Shift
-                    events = [
-                        libevdev.InputEvent(libevdev.EV_KEY.KEY_LEFTSHIFT, 1),
-                        libevdev.InputEvent(libevdev.EV_SYN.SYN_REPORT, 0)
-                    ]
-                    fake_kbd.send_events(events)
-
-                    # press Meta
-                    events = [
-                        libevdev.InputEvent(libevdev.EV_KEY.KEY_LEFTMETA, 1),
-                        libevdev.InputEvent(libevdev.EV_SYN.SYN_REPORT, 0)
-                    ]
-                    fake_kbd.send_events(events)
-
-                    # press R
-                    events = [
-                        libevdev.InputEvent(libevdev.EV_KEY.KEY_R, 1),
-                        libevdev.InputEvent(libevdev.EV_SYN.SYN_REPORT, 0)
-                    ]
-                    fake_kbd.send_events(events)
-
-                    # release R
-                    events = [
-                        libevdev.InputEvent(libevdev.EV_KEY.KEY_R, 0),
-                        libevdev.InputEvent(libevdev.EV_SYN.SYN_REPORT, 0)
-                    ]
-                    fake_kbd.send_events(events)
-
-                    # release Meta
-                    events = [
-                        libevdev.InputEvent(libevdev.EV_KEY.KEY_LEFTMETA, 0),
-                        libevdev.InputEvent(libevdev.EV_SYN.SYN_REPORT, 0)
-                    ]
-                    fake_kbd.send_events(events)
-
-                    # release Shift
-                    events = [
-                        libevdev.InputEvent(libevdev.EV_KEY.KEY_LEFTSHIFT, 0),
-                        libevdev.InputEvent(libevdev.EV_SYN.SYN_REPORT, 0)
-                    ]
-                    fake_kbd.send_events(events)
-                except OSError as e:
-                    pass
+                # release Shift
+                events = [
+                    libevdev.InputEvent(libevdev.EV_KEY.KEY_LEFTSHIFT, 0),
+                    libevdev.InputEvent(libevdev.EV_SYN.SYN_REPORT, 0)
+                ]
+                fake_kbd.send_events(events)
+            except OSError as err:
+                pass
 
         # if it's the "MyAsus" key
-        elif e.value == KEY_WMI_MYASUS:
+        elif event.value == KEY_WMI_MYASUS:
+            logging.debug('MyAsus key pressed')
             try:
 
                 # press Shift
@@ -271,7 +208,7 @@ while True:
                     libevdev.InputEvent(libevdev.EV_SYN.SYN_REPORT, 0)
                 ]
                 fake_kbd.send_events(events)
-            except OSError as e:
+            except OSError as err:
                 pass
 
         # not our key
