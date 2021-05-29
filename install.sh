98fc1069189fa8231cf6aa222a4ff1ed28292040
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2021-05-28 22:59:53 -0400

    minor eidts

--- install.sh
+++ install.sh
@@ -11,22 +11,22 @@
 
 # show some progress
 # NB: first call with sudo to ask for password on its own line (aesthetics)
-sudo echo "Installing Asus_L410M_WMI_Keys..."
+sudo echo "Installing Asus_L410M_WMI_Keys... "
 echo "For license info see the LICENSE.txt file in this directory"
 
 # install dependencies
-esudo cho "Installing dpendencies..."
+sudo echo "Installing dpendencies... "
 sudo apt-get install python3-libevdev
 echo "Done"
 
 # copy files to location
-echo -n "Copying files..."
+echo -n "Copying files... "
 sudo cp ./asus_l410m_wmi_keys.py /usr/bin
 sudo cp ./asus_l410m_wmi_keys.service /lib/systemd/system/
 echo "Done"
 
 # start service now and on reboot
-echo -n "Starting service..."
+echo "Starting service... "
 sudo systemctl start asus_l410m_wmi_keys
 sudo systemctl enable asus_l410m_wmi_keys
 echo "Done"
