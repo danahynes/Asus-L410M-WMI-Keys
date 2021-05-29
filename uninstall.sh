98651d828211916b474f043c057f54b29cf4d0cc
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2021-05-28 22:56:28 -0400

    added some output messages

--- uninstall.sh
+++ uninstall.sh
@@ -7,15 +7,23 @@
 # License : WTFPLv2                                              \          /  #
 #------------------------------------------------------------------------------#
 
-# N.B. doesn't matter if run as sudo or not
+# NB: doesn't matter if run as sudo or not
+
+# show some progress
+# NB: first call with sudo to ask for password on its own line (aesthetics)
+sudo echo "Uninstalling Asus_L410M_WMI_Keys..."
 
 # stop service now and on reboot
+echo -n "Stopping service..."
 sudo systemctl stop asus_l410m_wmi_keys
 sudo systemctl disable asus_l410m_wmi_keys
+echo "Done"
 
 # delete files from location
+echo -n "Deleting files..."
 sudo rm -rf /usr/bin/asus_l410m_wmi_keys.py
 sudo rm -rf /lib/systemd/system/asus_l410m_wmi_keys.service
 sudo rm -rf /var/log//asus_l410m_wmi_keys.log
+echo "Done"
 
 # -)
