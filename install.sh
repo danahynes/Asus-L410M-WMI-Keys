851af0febd00f288abdd281a9e56730e6728bb98
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2021-05-28 22:56:13 -0400

    added some output messages

--- install.sh
+++ install.sh
@@ -7,17 +7,28 @@
 # License : WTFPLv2                                              \          /  #
 #------------------------------------------------------------------------------#
 
-# N.B. doesn't matter if run as sudo or not
+# NB: doesn't matter if run as sudo or not
+
+# show some progress
+# NB: first call with sudo to ask for password on its own line (aesthetics)
+sudo echo "Installing Asus_L410M_WMI_Keys..."
+echo "For license info see the LICENSE.txt file in this directory"
 
 # install dependencies
+esudo cho "Installing dpendencies..."
 sudo apt-get install python3-libevdev
+echo "Done"
 
 # copy files to location
+echo -n "Copying files..."
 sudo cp ./asus_l410m_wmi_keys.py /usr/bin
 sudo cp ./asus_l410m_wmi_keys.service /lib/systemd/system/
+echo "Done"
 
 # start service now and on reboot
+echo -n "Starting service..."
 sudo systemctl start asus_l410m_wmi_keys
 sudo systemctl enable asus_l410m_wmi_keys
+echo "Done"
 
 # -)
