32c72e70562b867fb68a7047ba76a2737b0069c3
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2021-05-28 23:00:03 -0400

    minor edits

--- uninstall.sh
+++ uninstall.sh
@@ -11,16 +11,16 @@
 
 # show some progress
 # NB: first call with sudo to ask for password on its own line (aesthetics)
-sudo echo "Uninstalling Asus_L410M_WMI_Keys..."
+sudo echo "Uninstalling Asus_L410M_WMI_Keys... "
 
 # stop service now and on reboot
-echo -n "Stopping service..."
+echo "Stopping service... "
 sudo systemctl stop asus_l410m_wmi_keys
 sudo systemctl disable asus_l410m_wmi_keys
 echo "Done"
 
 # delete files from location
-echo -n "Deleting files..."
+echo -n "Deleting files... "
 sudo rm -rf /usr/bin/asus_l410m_wmi_keys.py
 sudo rm -rf /lib/systemd/system/asus_l410m_wmi_keys.service
 sudo rm -rf /var/log//asus_l410m_wmi_keys.log
